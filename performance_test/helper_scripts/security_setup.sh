bfe4060a50aad33028be8384ea548f9854421790
Sumanth Nirmal <sumanth.nirmal@apex.ai>
2018-08-28 13:14:28 -0700

    updated the script for sourcing

--- performance_test/helper_scripts/security_setup.sh
+++ performance_test/helper_scripts/security_setup.sh
@@ -1,49 +1,53 @@
 #!/bin/bash
 
+
 set -e
 
 # help
-if [ "$1" = "--help" ] || [ -z "$1" ] ; then
-  echo "Usage: enable_security <root_directory_to_keys> <enable/disable>"
+if [ "$1" = "--help" ] || [ -z "$1" ] || [[ "$1" == "enable" && -z "$2" ]] || [[ "$1" != "disable" && "$1" != "enable" ]]
+then
+#  ; then
+  echo "Usage: setup_security <enable/disable> <root_directory_to_keys>"
+  echo "directory to keys is required when security is enabled"
   echo "Example usage: "
-  echo "echo enable_security '~/ApexOS/apex_ws/demo_keys' enable"
+  echo "To enable the security: "
+  echo "bash setup_security.sh enable '/ApexOS/apex_ws/demo_keys'"
+  echo "To disable the security: "
+  echo "bash setup_security.sh disable"
   exit 0
 fi
 
-
-if [ $2 == 'enable' ]
+if [ "$1" == "enable" ]
 then 
-  echo 'enabling security'
+  echo 'generating keys'
 
   max_nodes=5
 
   source ~/ApexOS/apex_ws/install/setup.bash  
 
   # create keys
-  ros2 security create_keystore "$1"
+  ros2 security create_keystore "$2"
 
   for ((i=0; i<=$max_nodes; i++))
   do
     node=performance_test$i
     echo "generating keys for "$node
-    echo $1
-    ros2 security create_key "$1" "$node"
-    ros2 security create_permission "$1" "$node" policies.yaml;  
+    ros2 security create_key "$2" "$node"
+    ros2 security create_permission "$2" "$node" policies.yaml;  
     echo " "
   done 
 
   #enable security
-  export ROS_SECURITY_ENABLE=true 
-  export ROS_SECURITY_STRATEGY=Enforce 
-  export ROS_SECURITY_ROOT_DIRECTORY=$1 
   source ~/ApexOS/apex_ws/install/setup.bash
-else 
-  echo 'diabling security'
+  echo "run this line to enable security"
+  echo "export ROS_SECURITY_ENABLE=true && export ROS_SECURITY_STRATEGY=Enforce && export ROS_SECURITY_ROOT_DIRECTORY=$2"
+elif [ "$1" == "disable" ]
+then 
+  echo "run this line to disable security"
   #disable security
-  unset ROS_SECURITY_ENABLE 
-  unset ROS_SECURITY_STRATEGY 
-  unset ROS_SECURITY_ROOT_DIRECTORY
-  echo 'security disabled'
+  echo "unset ROS_SECURITY_ENABLE && unset ROS_SECURITY_STRATEGY && unset ROS_SECURITY_ROOT_DIRECTORY"
+else 
+  echo 'incorrect parameter '$2
 fi
 
 
