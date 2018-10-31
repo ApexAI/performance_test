6b85ecc69c5c4175558c7e805c23a10962627109
Abby Xu <abbyxu@amazon.com>
2018-10-30 18:12:03 -0700

    changed shebang line position

--- performance_test/helper_scripts/performance_test_file_reader.py
+++ performance_test/helper_scripts/performance_test_file_reader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright 2017 Apex.AI, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/usr/bin/env python
 
 # flake8: noqa This file is for plotting data. Its dependencies are not necessarily on the CI.
 import os
