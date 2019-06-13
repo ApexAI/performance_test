b9974499881386e0b9d887758be04f17455b3cc8
Esteve Fernandez <esteve@apache.org>
2019-06-13 15:53:00 +0200

    Fix style issues

--- performance_test/test/66_qos_liveliness_test.py
+++ performance_test/test/66_qos_liveliness_test.py
@@ -1,3 +1,21 @@
+# Copyright 2019 Apex.AI, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import ament_index_python
+
 import launch
 import launch.actions
 
@@ -21,6 +39,7 @@ perf_test_process_relay = launch.actions.ExecuteProcess(
     env=proc_env,
 )
 
+
 def generate_test_description(ready_fn):
 
     return launch.LaunchDescription([
@@ -29,4 +48,4 @@ def generate_test_description(ready_fn):
 
         # Start tests right away - no need to wait for anything
         launch.actions.OpaqueFunction(function=lambda context: ready_fn()),
-])
+    ])
