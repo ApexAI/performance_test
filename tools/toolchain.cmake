1356cdf15e1bf5f521840e5556ad20a19c218a25
Pete Baughman <pete.baughman@apex.ai>
2019-10-10 12:04:45 -0700

    Allow use of another SYSROOT
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>

--- tools/toolchain.cmake
+++ tools/toolchain.cmake
@@ -1,8 +1,17 @@
 set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
-set(CMAKE_SYSROOT /sysroot/)
 
-set(CMAKE_FIND_ROOT_PATH /sysroot)
+if(DEFINED ENV{SYSROOT})
+  set(_SYSROOT $ENV{SYSROOT})
+else()
+  # Github CI expects this to be set automatically.  Other CI might want to set its own
+  # SYSROOT
+  set(_SYSROOT /sysroot)
+endif()
+
+set(CMAKE_SYSROOT ${_SYSROOT})
+
+set(CMAKE_FIND_ROOT_PATH ${_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
